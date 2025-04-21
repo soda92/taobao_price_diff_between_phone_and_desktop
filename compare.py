7e35eabd5285fb63852e1012d4148951a8fa3564
Maple <wjxa20152015@gmail.com>
2025-04-21 23:37:39 +0800

    minor fix

--- compare.py
+++ compare.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 # %%
 
 data_desktop = pd.read_csv("desktop_prices.txt", header=None)
-data_desktop = data_desktop[data_desktop<400]
+data_desktop = data_desktop[data_desktop < 400]
 data_desktop = data_desktop.dropna()
 # %%
 data_phone = pd.read_csv("phone_prices.txt", header=None)
@@ -29,10 +29,14 @@ data_phone.describe()
 # %%
 
 
-plt.figure(figsize=(2, 4))
+plt.figure(figsize=(3, 6))
 
 
-plt.boxplot([data_desktop.iloc[:,0], data_phone.iloc[:,0]], labels=['desktop', 'phone'], widths=[0.5, 0.5])
+plt.boxplot(
+    [data_desktop.iloc[:, 0], data_phone.iloc[:, 0]],
+    labels=["desktop", "phone"],
+    widths=[0.7, 0.7],
+)
 plt.savefig("box_plot.png")
 
 # %%
@@ -44,8 +48,8 @@ data_phone.mean()
 # %%
 data_phone.mean() - data_desktop.mean()
 # %%
-higher = (data_phone.mean() - data_desktop.mean())/data_desktop.mean()
+higher = (data_phone.mean() - data_desktop.mean()) / data_desktop.mean()
 # float(higher)
 # %%
-print(f"phone prices are higher than desktop for {higher.iloc[0]*100:.0f}% percent.")
+print(f"phone prices are higher than desktop for {higher.iloc[0] * 100:.0f}% percent.")
 # %%
